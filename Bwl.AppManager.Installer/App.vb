41706157807706e74551ba1da84efb4eef019ff9
Igor Koshelev <igor@cleverflow.ru>
2016-11-25 15:08:20 +0300

    Installer.

--- Bwl.AppManager.Installer/App.vb
+++ Bwl.AppManager.Installer/App.vb
@@ -1,3 +1,10 @@
 ﻿Module App
 
+    Public Sub Main()
+        InstallTools.AssertGitInstalled()
+        Dim appPath As String = IO.Path.Combine(System.Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Bwl AppManager", "Bwl.AppManager")
+        Dim appName = "Bwl AppManager"
+        Dim url = "https://github.com/Lifemotion/Bwl.AppManager"
+        InstallTools.InstallFromRepository(appPath, url, appName)
+    End Sub
 End Module
---
+++ Bwl.AppManager.Installer/InstallerTools/InstallTools.vb
@@ -0,0 +1,47 @@
+﻿Public Class InstallTools
+    Public Shared Sub CreateShortcut(path As String, targetPath As String)
+        Dim wsh As Object = CreateObject("WScript.Shell")
+        wsh = CreateObject("WScript.Shell")
+        Dim MyShortcut = wsh.CreateShortcut(path)
+        MyShortcut.TargetPath = targetPath
+        MyShortcut.WorkingDirectory = IO.Path.GetDirectoryName(targetPath)
+        MyShortcut.WindowStyle = 4
+        MyShortcut.Save()
+    End Sub
+
+    Public Shared Sub AssertGitInstalled()
+        Try
+            GitTool.Init()
+        Catch ex As Exception
+            MsgBox("Git not installed. Git required to work. Please download git at: " + vbCrLf + "https://git-scm.com/", MsgBoxStyle.Exclamation)
+            Return
+        End Try
+    End Sub
+
+    Public Shared Sub InstallFromRepository(appPath As String, cloneUrl As String, appName As String)
+        InstallTools.AssertGitInstalled()
+        MsgBox(appName + " will be downloaded and installed")
+
+        GitTool.RepositoryPullOrClone(appPath, cloneUrl)
+
+        For Each sol In IO.Directory.GetFiles(appPath, "*.sln")
+            Try
+                Dim bh As New BuildHelper(sol, "Release")
+                bh.Build("")
+            Catch ex As Exception
+            End Try
+        Next
+
+        Dim foundExe As Boolean
+        For Each exe In IO.Directory.GetFiles(IO.Path.Combine(appPath, "release"), "*.exe", IO.SearchOption.AllDirectories)
+            Dim lnkName = IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), IO.Path.GetFileNameWithoutExtension(exe)) + ".lnk"
+            InstallTools.CreateShortcut(lnkName, exe)
+            foundExe = True
+        Next
+        If foundExe Then
+            MsgBox(appName + " installed sucessfully!", MsgBoxStyle.Information)
+        Else
+            MsgBox("Install failed, no EXE found after pull & build", MsgBoxStyle.Exclamation)
+        End If
+    End Sub
+End Class
